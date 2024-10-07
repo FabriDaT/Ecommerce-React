87731d133837613b91d826dad85beaf57f98d369
Fabricio Daniel Turrina <fabriciodanielturrina@gmail.com>
2024-10-07 12:52:45 -0300

    first commit

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import Layout from "../../Components/Layout";
+import Card from "../../Components/Card";
+
+function Home() {
+
+  const apiUrl = "https://api.escuelajs.co/api/v1/products";
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((responde) => responde.json())
+      .then((data) => setItems(data))
+      .catch((error) =>
+        console.error(`Ups! Ocurrio el siguiente error: ${error}`)
+      );
+  }, []);
+
+  return (
+    <Layout>
+      HOME HOME HOME HOME HOME HOME
+      <section className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+        {items?.map((item) => (
+          <Card key={item.id} data={item} />
+        ))}
+      </section>
+    </Layout>
+  );
+}
+
+export default Home;
