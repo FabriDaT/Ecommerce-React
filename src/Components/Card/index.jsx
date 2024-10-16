ed1a9a8895a867c70a3a56f918fa08eb06819732
Fabricio Daniel Turrina <fabriciodanielturrina@gmail.com>
2024-10-16 12:31:21 -0300

    first commit

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -3,28 +3,44 @@ import { useContext } from "react";
 import { ShoppingCartContext } from "../../Context";
 
 const Card = (data) => {
-
- // const context =  useContext(ShoppingCartContext)
-  const { count, setCount } = useContext(ShoppingCartContext)
+  // const context =  useContext(ShoppingCartContext)
+  const { count, setCount, openProductDetail } = useContext(ShoppingCartContext);
 
   return (
-    
-    <div className="bg-white cursor-pointer w-56 h-60 rounded-lg">
+    <div
+     className="bg-white cursor-pointer w-56 h-60 rounded-lg"
+     onClick={()=> openProductDetail()}
+     >
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5">
           {data.data?.category?.name}
         </span>
-        <img className="w-full h-full object-cover rounded-lg" src={data.data?.images[0]} alt={data.data.title} />
+        <img
+          className="w-full h-full object-cover rounded-lg"
+          src={data.data?.images[0]}
+          alt={data.data.title}
+        />
         <button
-         className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 m-2 p-1 hover:bg-blue-500 rounded-full font-bold"
-         // onClick={()=>{context.setCount(context.count +1)}}
-         onClick={() => setCount(count + 1)}
-         >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 100 100" strokeWidth={3.5} stroke="currentColor" className="w-full h-full">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-</svg>
-
-        </button> 
+          className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 m-2 p-1 hover:bg-green-500 rounded-full font-bold"
+          // onClick={()=>{context.setCount(context.count +1)}}
+          onClick={(e) =>{ e.stopPropagation()
+             setCount(count + 1)}}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 4.5v15m7.5-7.5h-15"
+            />
+          </svg>
+        </button>
       </figure>
       <p className="flex justify-between">
         <span className="text-sm font-light">{data.data?.title}</span>
--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import './styles.css'
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../Context";
+
 
 const ProductDetail = () => {
+  
+  const context =  useContext(ShoppingCartContext)
   return (
-    <aside className='product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white '>
+    <aside className={`${context.isProductDetailOpen? 'flex' : 'hidden'} product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white `}>
         <div className='flex items-center justify-between p-6'>
             <h2 className='font-medium text-xl'> Detail </h2>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <button onClick={context.closeProductDetail}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="m9.75 9.75 4.5 4.5m0-4.5-4.5 4.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
 </svg>
 
+            </button>
+          
 
             
         </div>
