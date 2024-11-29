7363473a6aef4f726e627cc48b30a3634edf708c
Fabricio Daniel Turrina <fabriciodanielturrina@gmail.com>
2024-11-29 12:59:53 -0300

    first commit

--- src/Pages/MyOrder/index.jsx
+++ src/Pages/MyOrder/index.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import Layout from '../../Components/Layout'
+import { useContext } from "react";
+import { ShoppingCartContext } from "../../Context";
+import OrderCard from '../../Components/OrderCard';
 
 function MyOrder() {
+
+  const context = useContext(ShoppingCartContext);
+
   return (
-    <Layout>MyOrder</Layout>
+    
+    <Layout>MyOrder
+
+   <div className="flex flex-col">
+        {context.order?.slice(-1)[0].s.map((product) => (
+          <OrderCard
+            id={product.id}
+            key={product.id} // use como 'key' el titulo ya que en la api se repiten los ID,
+            // cosa que da un error en consola al mapear los productos y repetirse la key
+            title={product.title}
+            imageUrl={product.images}
+            price={product.price}
+            quantity={product.quantity}
+           
+          />
+        ))}
+      </div>
+
+    </Layout>
   )
 }
 
